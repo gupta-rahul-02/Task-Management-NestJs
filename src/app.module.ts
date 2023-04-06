7548a0b83e6037621c82f5bea0acc5ddf1f49b66
rahul gupta <rahulguptaslg20@gmail.com>
2023-04-07 01:34:35 +0530

    Connection with postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [TasksModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'India@11',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ]
+  
 })
 export class AppModule {}
